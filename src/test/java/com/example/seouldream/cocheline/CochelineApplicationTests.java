3227b42f2f6cdfd168beb6e9b3451ee593ba3f96
myunghoonkim <xcv8826@gmail.com>
2022-10-28 21:03:57 +0900

    projectSettin
    initial commit

---
+++ src/test/java/com/example/seouldream/cocheline/CochelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seouldream.cocheline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CochelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
